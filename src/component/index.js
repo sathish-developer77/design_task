f2a3ab01cda998a8ba6f0c80ff200fb3ae7d8485
Sathishkumar Jaganathan <sathishkumarjaganathan@AGLS-MLT-498.local>
2022-03-01 17:41:50 +0530

    design_task

---
+++ src/component/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar1 from './Navbar';
+
+import Maincontent from './mainContent';
+
+import '../css/style.css';
+
+
+const Index = () => {
+  return (
+    <div>
+
+        
+          <Navbar1 />
+          <Maincontent />
+           
+       
+    </div>
+  )
+}
+
+export default Index
